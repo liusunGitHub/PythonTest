352093a43f8a073c33b3c2d5d2c8519554abadf5
liuguosun <474882245@qq.com>
2018-10-25 18:09:41 +0800

    更新

--- Include/JobTest.py
+++ Include/JobTest.py
@@ -1,8 +1,16 @@
 def test12():
+    '''
+    在不用其他变量的情况下，交换a、b变量的值
+    '''
     a = 1
     b = 2
+    print("a、b变量的值")
+    print(" a = " + str(a))
+    print(" b = " + str(b))
     a, b = b, a
-    print("a= " + str(a))
-    print("b= " + str(b))
+    print("")
+    print("交换a、b变量后的值")
+    print(" a = " + str(a))
+    print(" b = " + str(b))
 
 test12()
