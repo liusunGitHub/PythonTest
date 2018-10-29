a7d3756aefa5cfb3aa5e748e5d76e6983bb08e2f
liuguosun <474882245@qq.com>
2018-10-29 20:32:40 +0800

    服务器

--- Include/client3_test_tcp.py
+++ Include/client3_test_tcp.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 '''
- 客户端1,功能：计算奇偶性
+ 客户端3,功能：计算数字 是否大于10
 '''
 
 # 导入包
@@ -11,9 +11,8 @@ import socket, time
 recv_num = ""
 # 计算数字后，发送给服务器的消息
 send_msg = ""
-
 # 为了打印日志更好看
-tag = "**************".encode()
+tag = "---------------------".encode()
 
 # 创建套接字
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -23,13 +22,13 @@ sock.connect(("127.0.0.1", 9000))
 # 循环发送接收消息，客户端先发消息
 while True:
     if recv_num == "":
-        send_msg = tag + "我是客户端1".encode();  # 客户端先发第一条消息通知服务器，准备接收消息
+        send_msg = tag + "我是客户端3".encode();  # 客户端先发第一条消息通知服务器，准备接收消息
     else:
         print(recv_num)  # 打印服务器发送的数字
-        if int(str(recv_num)) % 2 == 0:  # 接收到服务器的数字为偶数,发送给服务器的消息： 数字 False
-            send_msg = tag + " False".encode() + str(recv_num).encode()
+        if int(str(recv_num)) > 10:  # 接收到服务器的数字为偶数,发送给服务器的消息： 数字 False
+            send_msg = tag + ">10 : False ".encode() + str(recv_num).encode()
         else:  # 接收到服务器的数字为偶数,发送给服务器的消息： 数字 True
-            send_msg = tag + " True".encode() + str(recv_num).encode()
+            send_msg = tag + ">10 : True  ".encode() + str(recv_num).encode()
     sock.send(send_msg)  # 发送数据
     time.sleep(2)
     recv_num = sock.recv(521).decode()
--- Include/server_test_tcp.py
+++ Include/server_test_tcp.py
@@ -1,36 +1,40 @@
-#coding=utf-8
+# coding=utf-8
 
-#导入包
-import socket,time,random,threading
+# 导入包
+import socket, time, random, threading
 
-numCount = 30;#用来计算的随机数的总数
+numCount = 30;  # 用来计算的随机数的总数
 
-#列表需要(31)随机数用来接收第30个元素的计算消息
-random_list = [random.randrange(1, 1000) for i in range(numCount+1)]
+# 通过推导式，生产31个随机数，其中前30个数字发送给客户端，并返回30计算后的数字，最后一个只是用来显示第30个数的计算结果
+random_list = [random.randrange(1, 1000) for i in range(numCount + 1)]
 
-#接受或者发送信息的函数
-def setResponse(sock,addresss):
+
+# 接受或者发送信息的函数
+def msgResponse(sock, addresss):
+    print('%s:%s is connected' % (addresss))
+    # 循环31次给客户端发31个数，最后一个数不会返回服务器，--因为客户端要先发个消息，第一次接收的消息不是计算的数字
     for i in random_list:
-        recv_msg = sock.recv(521).decode()
-        print(recv_msg)
-        #向客户端发送信息
+        recv_msg = sock.recv(521).decode()  # 服务器先接收客户端的会话
+        print(recv_msg)  # 显示客户端发的消息
+        # 向客户端发送数字，字符串的形式，encode()
         sock.send(str(i).encode())
         time.sleep(2)
 
-#创建套接字
+
+# 创建套接字
 _sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-_sock.bind(('',9000))
+# 绑定端口
+_sock.bind(('', 9000))
+
+# 监听端口
 _sock.listen(5)
-#死循环，开启线程
+
+# 死循环，开启线程
 while True:
-    client,address =_sock.accept()
-    t = threading.Thread(target=setResponse,args=(client,address))
+    client, address = _sock.accept()
+    t = threading.Thread(target=msgResponse, args=(client, address))
     t.start()
-#关闭套接字
+
+# 关闭套接字
 _sock.close()
-
-
-
-
-
